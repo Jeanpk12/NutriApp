7bdbb5b3721dd01c507d81ccb232bf806fa4b173
Jeanpk12 <jeanolivera123@gmail.com>
2024-02-09 02:09:37 -0300

    NutriApp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import './style.css' 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+function App(){
+
+  const[nutri, setNutri] = useState([]);
+
+  useEffect(()=> {
+
+    function callAPI(){
+      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
+      
+      fetch(url)
+      .then((r)=> r.json())
+      .then((json)=> {
+        
+        setNutri(json)
+      })
+    }
+
+    callAPI();
+
+  }, []);
+
+  return(
+    <div className="container">
+      <header>
+        <strong>Refeição Nutritiva</strong>
       </header>
+
+      {nutri.map((item)=>{
+        return(
+          <article key={item.id} className="post">
+            <strong className="titulo">{item.titulo}</strong>
+            <img src={item.capa} alt={item.titulo} className="capa" />
+
+            <p className="subtitulo">{item.subtitulo}</p>
+            <p className="categoria">#{item.categoria}</p>
+            <a href="#" className="btn">Acessar</a>
+          </article>
+        )
+      })}
     </div>
   );
 }
